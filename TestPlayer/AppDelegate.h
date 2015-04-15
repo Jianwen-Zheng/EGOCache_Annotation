19c97183c8b35d0241493715e627cedfaee36f95
臧其龙 <zangqilong@zangqilongdeMacBook-Pro.local>
2015-04-15 10:34:23 +0800

    Initial Commit

---
+++ TestPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestPlayer
+//
+//  Created by 臧其龙 on 15/4/15.
+//  Copyright (c) 2015年 臧其龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
